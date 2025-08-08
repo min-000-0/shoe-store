ac0d70eb855eee142845120e9afe6f781b75777c
Reina <two.oao@gmail.com>
2025-08-08 18:26:08 +0800

    update

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
-console.log('Hello world');
+console.log("Hello world");
+
+import Swiper from "swiper";
+import "swiper/css";
+
+new Swiper(".swiper-container", {
+  loop: true,
+});
